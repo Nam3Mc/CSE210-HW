9720b69199acf39f1f07d32c599bc5de09d5f640
Nam3Mc <mor23083@byui.edu>
2024-01-30 20:36:33 -0500

    done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment ( "Dreiser Morales", "Programing" ) ;
+        Console.WriteLine ( assignment1.GetSummary ( ) ) ;
+
+        MathAssignment math1 = new MathAssignment ( "Juan Muñoz", "Divition", "36/5", "87/26" ) ;
+        Console.WriteLine ( math1.GetHomeworkList ( ) ) ;
+
+        WritingAssignment writing1 = new WritingAssignment ( "Eva Perez", "Religion", "God Is Not Dead" ) ;
+        Console.WriteLine ( writing1.GetStudentName ( ) ) ;
     }
 }
